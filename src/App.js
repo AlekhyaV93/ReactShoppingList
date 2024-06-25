91893be52815666477f05514beacf5d9bb0a354f
Alekhya <vinnakotaalekhya@gmail.com>
2024-06-24 21:14:06 -0500

    Adding items to list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+function AddToList({textValue,setul,addTextValue,listItemValue}){ 
+  return(
+    <>
+      <form>
+      <input type='text'className='task' placeholder='Enter the items' required value={textValue} onChange={(e)=>addTextValue(e.target.value)} ></input>
+      <button onClick={(e)=>handleClick(textValue,e)}>Add</button>
+      </form>
+      <br/>
+    </>
+  )
+  function handleClick(value,e){
+    e.preventDefault();
+    if(value  !== ''){
+      setul([...listItemValue,value]);
+      console.log(listItemValue);
+     }
+  }
+
+}
+function ListOut({listItemValue}){
+  /* Add key to li and delete that particular li */
+  return(
+    <>
+      <ul>  
+      {listItemValue.map(item => <li>{item} 
+      <button>Delete</button>
+      </li>)}
+      </ul>
+    </>
+  )
+}
 function App() {
+  const [textValue,setTextValue]= useState('');
+  const [listItemValue,setListItemValue] = useState([]);
+  /*function setTextValue(){
+
+  }*/
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddToList textValue={textValue} setul={setListItemValue} addTextValue={setTextValue} listItemValue={listItemValue}/>
+      <ListOut listItemValue={listItemValue}/>
+      
     </div>
   );
 }
